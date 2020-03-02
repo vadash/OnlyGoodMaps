7d9b25bb740a965060095971f056388b2a79dfd1
vadash <vadash@gmail.com>
2020-03-02 19:23:35 +0300

    Initial commit

---
+++ Core.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExileCore;
+using ExileCore.Shared;
+using ExileCore.Shared.Enums;
+using SharpDX;
+using ImGuiNET;
+
+// ReSharper disable IteratorNeverReturns
+// ReSharper disable UnusedMember.Local
+// ReSharper disable RedundantExtendsListEntry
+// ReSharper disable UnusedType.Global
+
+namespace LazyPricer
+{
+    [Obfuscation(Feature = "Apply to member * when constructor: virtualization", Exclude = false)]
+    public partial class Core : BaseSettingsPlugin<Settings>
+    {
+        private Coroutine _mainCoroutine;
+
+        public Core()
+        {
+            Name = "LazyPricer";
+        }
+
+        [Obfuscation(Feature = "virtualization", Exclude = false)]
+        public override bool Initialise()
+        {
+            return true;
+        }
+        
+        public override void Render()
+        {
+        }
+    }
+}
---
+++ ObfuscationSettings.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: Obfuscation(Feature = @"assembly probing path C:\projects\PoeHelper")]
+[assembly: Obfuscation(Feature = @"code control flow obfuscation", Exclude = false)]
+[assembly: Obfuscation(Feature = @"encrypt symbol names with password BmqW9YXC3czAZ3=T?DZWAWK7Ru$JTFr2", Exclude = false)]
+[assembly: Obfuscation(Feature = @"debug [secure]", Exclude = false)]
+[assembly: Obfuscation(Feature = @"encrypt resources [compress]", Exclude = false)]
+[assembly: Obfuscation(Feature = @"ignore InternalsVisibleToAttribute", Exclude = false)]
+[assembly: Obfuscation(Feature = @"merge with Auz.dll", Exclude = false)]
